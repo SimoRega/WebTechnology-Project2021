0091ab0990fb216de96296e77a345156a120b8c2
SimoRega <simone.rega2@studio.unibo.it>
2021-11-28 22:32:05 +0100

    Aggiunto php, piccola prova

---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+#require_once("utils.php");
+
+$templateParams["title"] = "CamperRomagna - Home";
+$templateParams["template"] = "index-template.php";
+
+require("../templates/base.php");
+?>
---
+++ php/utils/utils.php
@@ -0,0 +1,5 @@
+<?php
+require_once("database.php");
+#$db = new DatabaseConnection("localhost", "root", "", "camperromagna", 3306);
+#define("UPLOAD_DIR", "./upload/")
+?>
