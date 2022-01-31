9075321e300e6ef7d03597d7d43ce8638ac79098
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2022-02-01 00:52:45 +0100

    added filter on shop

--- php/templates/shop-item.php
+++ php/templates/shop-item.php
@@ -1,4 +1,14 @@
-<?php $item = $templateParams["articolo"][0] ?>
+
+<?php
+
+    if(!isset($_GET["id"])){
+        echo "missing arguments";
+        return;
+    }
+
+    $idProdotto = $_GET['id'];
+    $item = $dbh->getItem($idProdotto)[0];
+?>
 
 
 <div class="container px-4 px-lg-5 my-5">
@@ -8,15 +18,18 @@
         </div>
         <div class="col-md-6">
 
-            <h1 class="display-5 fw-bolder"><?php echo $item["nome"]; ?></h1>
+            <h1 class="white display-5 fw-bolder"><?php echo $item["nome"]; ?></h1>
             <div class="fs-5 mb-5">
-                <span class="text-decoration-line-through">$45.00</span>
-                <span><?php echo $item["prezzo"]; ?></span>
+                <!--
+                    <span class="text-decoration-line-through">$45.00</span>
+                -->
+                <p class="white" ><?php echo $item["prezzo"]; ?>€</p>
             </div>
-            <p class="lead"><?php echo $item["descrizione"]; ?></p>
+
+            <p class="white lead"><?php  echo $item["descrizione"]; ?></p>
             <div class="d-flex">
                 <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem">
-                <button class="btn " type="button">
+                <button class="white btn " type="button">
                     <i class="bi-cart-fill me-1"></i>
                     Add to cart
                 </button>
