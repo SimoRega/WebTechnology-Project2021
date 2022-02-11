343ae776b6bd0dff3e074dd0263971fd79526423
Linda <linda.fabbri5@studio.unibo.it>
2022-02-11 11:00:13 +0100

    è tutto rotto

--- js/configuratore.js
+++ js/configuratore.js
@@ -1,9 +1,11 @@
 var totPag=0;
 var currentPage=0;
+var lastPrice=0;
 $(document).ready(inizializeConfiguratore());
 
 
 function inizializeConfiguratore(){
+    console.log("ciao");
     $(".sceltaConfiguratore").each(function(index){
         $(this).attr('id', index);
         $(this).toggle();
@@ -14,10 +16,11 @@ function inizializeConfiguratore(){
     $("#0").show();
     $( "#stepConfigurazione li" ).eq( currentPage ).addClass("text-danger");
     $("button[name=invia]").addClass("disabled");
+    $("button[name=indietro]").addClass("disabled");
 }
 
 $("button[name=avanti]").click(function(){
-    if(currentPage+1!==totPag){
+    if(currentPage!==totPag){
         $("#"+currentPage).hide();
         currentPage++;
         $("#"+currentPage).show();
@@ -46,3 +49,10 @@ $("button[name=indietro]").click(function(){
         $(this).addClass("disabled");
     }
 });
+
+
+function aggiungiPrezzo(){
+    var costo= $('input[name="color"]:checked small').text();
+    alert(costo);
+
+}
