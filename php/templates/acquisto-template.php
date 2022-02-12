3009a59f0ea855b462d13deb3f8c352051877cd0
raffo <federicoraffoni00@gmail.com>
2022-02-12 11:40:10 +0100

    acquisto

--- php/templates/acquisto-template.php
+++ php/templates/acquisto-template.php
@@ -7,16 +7,29 @@
       <div class="card m-3 p-2">
         <h5 class="mb-1 ">Indirizzo di Consegna</h5>
           <div class="form-floating m-2">
-            <input type="text" class="form-control" id="floatingInput" required name="nome" placeholder="mariorossi@gmail.com" >
-            <label for="floatingInput">Nome e cognome</label>
+            <input type="text" class="form-control" id="nome" required  name="nome" placeholder="Mario Rossi"/>
+            <label for="nome"> Nome e cognome</label>
+
           </div>
           <div class="form-floating m-2">
-            <input type="text" class="form-control" id="floatingInput" required name="citta" placeholder="Cesena">
-            <label for="floatingInput">Città</label>
+            <input type="text" class="form-control" id="city" required name="citta" placeholder="Cesena">
+            <label for="city">Città</label>
           </div>
-          <div class="form-floating m-2">
-            <input type="text" class="form-control" id="floatingInput" required name="via" placeholder="Via vulcano 10">
-            <label for="floatingInput">Via</label>
+          <div class="row">
+            <div class="col-8">
+
+              <div class="form-floating m-2">
+                <input type="text" class="form-control" id="via" required name="via" placeholder="Via vulcano 10">
+                <label for="via">Via</label>
+              </div>
+            </div>
+            <div class="col-4">
+
+              <div class="form-floating m-2">
+                <input type="text" class="form-control" id="cap" required name="cap" placeholder="Via vulcano 10">
+                <label for="cap">Cap</label>
+              </div>
+            </div>
           </div>
       </div>
 
@@ -28,6 +41,24 @@
             <input type="text" class="form-control" id="floatingInput" placeholder="carta" required name="carta" >
             <label for="floatingInput">Inserire la carta</label>
           </div>
+
+          <div class="row">
+            <div class="col">
+
+              <div class="form-floating m-2">
+                <input type="text" class="form-control" id="scadenza" required name="scadenza" placeholder="02/22">
+                <label for="scadenza">Scadenza</label>
+              </div>
+            </div>
+            <div class="col">
+
+              <div class="form-floating m-2">
+                <input type="text" class="form-control" id="pin" required name="pin" placeholder="555">
+                <label for="pin">Pin</label>
+              </div>
+            </div>
+          </div>
+
       </div>
 
       <div class="card m-3 p-2">
@@ -35,5 +66,9 @@
       </div>
 
     </form>
+
+
   </div>
 </div>
+
+
